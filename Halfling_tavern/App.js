06733954797b81dde23794d8bdd396b4ffc8baf4
Thomas <thomas1.vigier@epitech.eu>
2022-02-16 17:27:23 +0100

    Created SheetResume Componenrt and almost finished first page

--- Halfling_tavern/App.js
+++ Halfling_tavern/App.js
@@ -16,68 +16,39 @@ import {
   View,
   StyleSheet,
   Image,
+  TouchableOpacity,
 } from 'react-native';
 import { SquircleView } from 'react-native-figma-squircle'
+import SheetResume from './src/Components/SheetResume';
 
 const App: () => Node = () => {
 
   return (
     <Provider store={store}>
+      <View style={{
+        backgroundColor: "rgb(99,11,11)",
+        flex: 15
+        }}>
+        <Text
+            style={{marginTop: 10, fontSize: 60, textAlign: "center", fontFamily: "Ace_Records"}}> 
+            Characters
+          </Text>
+      </View>
       <SafeAreaView style={{
         flexDirection: "column",
-        alignItems: "center"
+        alignItems: "center",
+        flex: 85,
         }}>
-        <SquircleView
-          style={{ width: "90%", height: 130 }}
-          squircleParams={{
-            cornerSmoothing: 0.7,
-            cornerRadius: 30,
-            fillColor: 'pink',
-          }}
+        <SheetResume name={"Doraka"} lvl={6} class={"Bard"} race={"Humain"}/>
+        <SheetResume name={"Islandzi"} lvl={6} class={"Rodeur"} race={"Elf"}/>
+        <TouchableOpacity
+          style={{top:360, left: 140}}
         >
-          <Text
-            style={{marginTop: 10, fontSize: 25, textAlign: "center"}}> 
-            Doraka 
-          </Text>
-          <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            alignItems: "center"
-            }}>
-            <Text
-              style={styles.MidleText}> 
-              lvl: 4 
-            </Text>
-            <Text
-              style={styles.MidleText}> 
-              Bard
-            </Text>
-            <Text
-              style={styles.MidleText}> 
-              Elf
-            </Text>
-          </View>
-          <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            alignItems: "center"
-            }}>
-              <Image
-                style={styles.tinyLogo}
-                source={require("./Ressources/amsterdam_city.jpg")}
-              />
-              <Image
-                style={styles.tinyLogo}
-                source={require("./Ressources/amsterdam_city.jpg")}
-              />
-              <Image
-                style={styles.tinyLogo}
-                source={require("./Ressources/amsterdam_city.jpg")}
-              />
-          </View>
-        </SquircleView>
+          <Image
+            style={styles.tinyLogo}
+            source={require("./Ressources/floating.png")}
+          />
+        </TouchableOpacity>
       </SafeAreaView>
     </Provider>
   );
@@ -87,6 +58,7 @@ const styles = StyleSheet.create({
   MidleText: {
     fontSize: 16,
     color: "black",
+    fontFamily: "Ace_Records"
   },
   tinyLogo: {
     width: 60,
