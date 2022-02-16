ee1dd6b8d37b321fc7d5d0e27efee774411ada58
Thomas <thomas1.vigier@epitech.eu>
2022-02-16 16:48:00 +0100

    First batch of sheet resume component

--- Halfling_tavern/App.js
+++ Halfling_tavern/App.js
@@ -13,21 +13,85 @@ import {store, persistor} from './Store/configureStore';
 import {
   SafeAreaView,
   Text,
+  View,
+  StyleSheet,
+  Image,
 } from 'react-native';
-
+import { SquircleView } from 'react-native-figma-squircle'
 
 const App: () => Node = () => {
 
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <Text>
-          Hello
-        </Text>
+      <SafeAreaView style={{
+        flexDirection: "column",
+        alignItems: "center"
+        }}>
+        <SquircleView
+          style={{ width: "90%", height: 130 }}
+          squircleParams={{
+            cornerSmoothing: 0.7,
+            cornerRadius: 30,
+            fillColor: 'pink',
+          }}
+        >
+          <Text
+            style={{marginTop: 10, fontSize: 25, textAlign: "center"}}> 
+            Doraka 
+          </Text>
+          <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center"
+            }}>
+            <Text
+              style={styles.MidleText}> 
+              lvl: 4 
+            </Text>
+            <Text
+              style={styles.MidleText}> 
+              Bard
+            </Text>
+            <Text
+              style={styles.MidleText}> 
+              Elf
+            </Text>
+          </View>
+          <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center"
+            }}>
+              <Image
+                style={styles.tinyLogo}
+                source={require("./Ressources/amsterdam_city.jpg")}
+              />
+              <Image
+                style={styles.tinyLogo}
+                source={require("./Ressources/amsterdam_city.jpg")}
+              />
+              <Image
+                style={styles.tinyLogo}
+                source={require("./Ressources/amsterdam_city.jpg")}
+              />
+          </View>
+        </SquircleView>
       </SafeAreaView>
     </Provider>
   );
 };
 
+const styles = StyleSheet.create({
+  MidleText: {
+    fontSize: 16,
+    color: "black",
+  },
+  tinyLogo: {
+    width: 60,
+    height: 60,
+  },
+});
 
 export default App;
